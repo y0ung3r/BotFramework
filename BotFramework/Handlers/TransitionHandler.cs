46ab40f6702656a17f74abac1b5d4193ccb9dd08
Никита Великий <mtyoungstz@outlook.com>
2021-11-03 17:50:25 +0500

    Добавлены комментарии в TransitionHandler

--- BotFramework/Handlers/TransitionHandler.cs
+++ BotFramework/Handlers/TransitionHandler.cs
@@ -4,12 +4,21 @@ using System.Threading.Tasks;
 
 namespace BotFramework.Handlers
 {
+    /// <summary>
+    /// Представляет переход запроса в указанный обработчик
+    /// </summary>
     internal class TransitionHandler : IRequestHandler
     {
         private readonly ILogger<TransitionHandler> _logger;
         private readonly RequestDelegate _from;
         private readonly RequestDelegate _to;
 
+        /// <summary>
+        /// Базовый конструктор
+        /// </summary>
+        /// <param name="logger">Сервис логгирования</param>
+        /// <param name="from">Обработчик из которого нужно осуществить переход</param>
+        /// <param name="to">Обработчик в который нужно осуществить переход</param>
         public TransitionHandler(ILogger<TransitionHandler> logger, RequestDelegate from, RequestDelegate to)
         {
             _logger = logger;
@@ -17,6 +26,11 @@ namespace BotFramework.Handlers
             _to = to;
         }
 
+        /// <summary>
+        /// Обработать запрос
+        /// </summary>
+        /// <param name="request">Запрос</param>
+        /// <param name="nextHandler">Следующий обработчик по цепочке</param>
         public Task HandleAsync(object request, RequestDelegate nextHandler)
         {
             throw new System.NotImplementedException();
