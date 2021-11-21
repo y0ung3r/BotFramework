726c7efcf272c4242ba8f49c31f276dd30ad5220
Никита Великий <mtyoungstz@outlook.com>
2021-11-22 00:36:38 +0500

    В проект добавлен объект, представляющий собой упакованные на выполнение пошаговые обработчики

---
+++ BotFramework/Handlers/StepHandlers/StepHandlerDistribution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using BotFramework.Handlers.Common.Interfaces;
+using BotFramework.Handlers.StepHandlers.Interfaces;
+
+namespace BotFramework.Handlers.StepHandlers
+{
+	/// <summary>
+	/// Представляет упакованные на выполнение пошаговые обработчики с уникальным ключом
+	/// </summary>
+	internal sealed class StepHandlerDistribution
+	{
+		/// <summary>
+		/// Уникальный ключ
+		/// </summary>
+		public object UniqueKey { get; }
+		
+		/// <summary>
+		/// Выполняемая команда
+		/// </summary>
+		public ICommandHandler Command { get; }
+		
+		/// <summary>
+		/// Обработчики, которые все еще были не выполнены
+		/// </summary>
+		public Stack<IStepHandler> RemainingHandlers { get; }
+		
+		/// <summary>
+		/// Запрос, который был обработан последним обработчиком
+		/// </summary>
+		public object PreviousRequest { get; }
+		
+		/// <summary>
+		/// Следующий на выполнение пошаговый обработчик
+		/// </summary>
+		public IStepHandler NextStepHandler { get; }
+	}
+}
