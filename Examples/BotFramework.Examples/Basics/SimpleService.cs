60f6095ddd4469d94ec4fe051bc4e6333106e357
Никита Великий <mtyoungstz@outlook.com>
2021-11-22 21:40:53 +0500

    Добавлена папка с примерами

---
+++ Examples/BotFramework.Examples/Basics/SimpleService.cs
@@ -0,0 +1,17 @@
+﻿using Basics.Interfaces;
+
+namespace Basics
+{
+	/// <summary>
+	/// Реализация для <see cref="ISimpleService" />
+	/// </summary>
+	public class SimpleService : ISimpleService
+	{
+		public bool BotFrameworkIsCool { get; }
+
+		public SimpleService()
+		{
+			BotFrameworkIsCool = true;
+		}
+	}
+}
