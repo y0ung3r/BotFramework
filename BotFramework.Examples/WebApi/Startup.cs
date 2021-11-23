9cfb8335b98ab5ce6c78135a31c6756ee90d3c02
Никита Великий <mtyoungstz@outlook.com>
2021-11-23 14:45:07 +0500

    Добавлен пример подключения бота к WebApi с помощью Telegram.Bot и веб-хуков

---
+++ BotFramework.Examples/WebApi/Startup.cs
@@ -0,0 +1,99 @@
+using BotFramework.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Telegram.Bot;
+using WebApi.Handlers;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        // Конфигурация бота из appsettings.json
+        public BotConfiguration BotConfiguration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            BotConfiguration = configuration.GetSection("BotConfiguration")
+                                            .Get<BotConfiguration>();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Внедрение WebHook'а
+            services.AddHostedService<WebhookConfiguration>();
+            
+            // Внедрение Telegram.Bot
+            services.AddHttpClient("tgwebhook")
+                    .AddTypedClient<ITelegramBotClient>
+                    (
+                        httpClient => new TelegramBotClient
+                        (
+                            BotConfiguration.Token, 
+                            httpClient
+                        )
+                    );
+            
+            // Внедрение BotFramework    
+            services.AddBotFramework()
+                    .AddHandler<EchoHandler>();
+            
+            services.AddControllers()
+                    .AddNewtonsoftJson();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc
+                (
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Title = "WebApi", 
+                        Version = "v1"
+                    }
+                );
+            });
+        }
+
+        // Большая часть параметров для настройки Telegram Bot API взята из https://github.com/TelegramBots/Telegram.Bot.Examples/tree/master/Telegram.Bot.Examples.WebHook
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage()
+                                  .UseSwagger()
+                                  .UseSwaggerUI
+                                  (
+                                      options => options.SwaggerEndpoint
+                                      (
+                                          "/swagger/v1/swagger.json", 
+                                          "WebApi v1"
+                                      )
+                                  );
+            }
+
+            applicationBuilder.UseHttpsRedirection()
+                              .UseRouting()
+                              .UseCors();
+
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute
+                (
+                    name: "tgwebhook",
+                    pattern: $"Bot/GetUpdates",
+                    new
+                    {
+                        сontroller = "Bot",
+                        action = "Post"
+                    }
+                );
+                
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ BotFramework.Examples/WebApi/WebhookConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace WebApi
+{
+    /// <summary>
+    /// См. https://github.com/TelegramBots/Telegram.Bot.Examples/blob/master/Telegram.Bot.Examples.WebHook/Services/ConfigureWebhook.cs
+    /// </summary>
+    public class WebhookConfiguration : IHostedService
+    {
+        private readonly BotConfiguration _botConfiguration;
+        private readonly ITelegramBotClient _client;
+
+        public WebhookConfiguration(IConfiguration configuration, ITelegramBotClient client)
+        {
+            _botConfiguration = configuration.GetSection("BotConfiguration")
+                                             .Get<BotConfiguration>();
+
+            _client = client;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var webhookAddress = @$"{_botConfiguration.WebHookAddress}/Bot/GetUpdates";
+            
+            await _client.SetWebhookAsync
+            (
+                url: webhookAddress,
+                allowedUpdates: Array.Empty<UpdateType>(),
+                cancellationToken: cancellationToken
+            );
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _client.DeleteWebhookAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
